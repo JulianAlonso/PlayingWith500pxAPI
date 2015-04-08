26be3f1f1d8b6caf8d38004635c467d2fa253dc6
Julian <julian.alonso.dev@gmail.com>
2015-04-08 16:53:17 +0200

    Added 500px ExternalPhotosProvider

---
+++ PlayingWith500px/Providers/FiveHungredpxExternalPhotosProvider.h
@@ -0,0 +1,14 @@
+//
+//  FiveHungredpxExternalPhotosProvider.h
+//  PlayingWith500px
+//
+//  Created by Julian Alonso on 8/4/15.
+//  Copyright (c) 2015 Julian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ExternalPhotosProvider.h"
+
+@interface FiveHungredpxExternalPhotosProvider : NSObject <ExternalPhotosProvider>
+
+@end
---
+++ PlayingWith500px/Providers/FiveHungredpxExternalPhotosProvider.m
@@ -0,0 +1,18 @@
+//
+//  FiveHungredpxExternalPhotosProvider.m
+//  PlayingWith500px
+//
+//  Created by Julian Alonso on 8/4/15.
+//  Copyright (c) 2015 Julian. All rights reserved.
+//
+
+#import "FiveHungredpxExternalPhotosProvider.h"
+
+@implementation FiveHungredpxExternalPhotosProvider
+
+- (void)loadPopularPhotosWithCompletion:(void (^)(NSArray *))completion
+{
+    
+}
+
+@end
