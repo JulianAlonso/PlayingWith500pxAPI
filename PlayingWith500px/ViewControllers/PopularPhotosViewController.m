023c4e4bcee85d72d1a200bf2cbb543cfa5a2586
Julian <julian.alonso.dev@gmail.com>
2015-04-08 19:42:18 +0200

    Preparing PopularPhotosviewcontroller

--- PlayingWith500px/ViewControllers/PopularPhotosViewController.m
+++ PlayingWith500px/ViewControllers/PopularPhotosViewController.m
@@ -9,9 +9,13 @@
 #import "PopularPhotosViewController.h"
 #import "LoadPopularPhotosInteractor.h"
 
-@interface PopularPhotosViewController ()
+NSString *const kPhotosPath = @"photos";
 
+@interface PopularPhotosViewController () <UICollectionViewDataSource, UICollectionViewDelegateFlowLayout>
+
+@property (weak, nonatomic) IBOutlet UICollectionView *photosCollectionView;
 @property (nonatomic, strong) NSArray *photos;
+@property (nonatomic, strong) NSString *controllers;
 
 @end
 
@@ -20,13 +24,18 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
+    [self registerToObservePhotos];
     [self loadPhotos];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
+}
+
+- (void)viewDidLayoutSubviews
+{
+    self.photosCollectionView.contentInset = UIEdgeInsetsMake(self.topLayoutGuide.length, 0, 0, 0);
 }
 
 #pragma mark - load methods.
@@ -42,4 +51,52 @@
     [self.loadPopularPhotosInteractor loadPopularPhotosWithCompletion:block andUpdate:block];
 }
 
+#pragma mark - Update methods.
+- (void)updateControllers
+{
+    
+}
+
+#pragma mark - Observe methods.
+- (void)registerToObservePhotos
+{
+    [self addObserver:self forKeyPath:kPhotosPath options:NSKeyValueObservingOptionInitial context:nil];
+}
+
+- (void)unregisterToObservePhotos
+{
+    [self removeObserver:self forKeyPath:kPhotosPath];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+    if ([keyPath isEqualToString:kPhotosPath])
+    {
+        [self updateControllers];
+        [self.photosCollectionView reloadData];
+    }
+}
+
+#pragma mark - DataSource methods.
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return nil;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return self.photos.count;
+}
+
+#pragma mark - LayoutDelegate methods.
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return CGSizeMake(200, 200);
+}
+
+#pragma mark - Dealloc methods.
+- (void)dealloc
+{
+    [self unregisterToObservePhotos];
+}
 @end
