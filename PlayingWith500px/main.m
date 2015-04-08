ce572d43cd24990f8c5dd0f371bbe5d7d11ba688
Julian <julian.alonso.dev@gmail.com>
2015-04-08 15:00:38 +0200

    Initial Commit

---
+++ PlayingWith500px/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PlayingWith500px
+//
+//  Created by Julian Alonso on 8/4/15.
+//  Copyright (c) 2015 Julian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
