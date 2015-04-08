a4cbc35293aecfa59be6399717a6dedf86dba01d
Julian <julian.alonso.dev@gmail.com>
2015-04-08 16:46:48 +0200

    Added APIVars

---
+++ PlayingWith500px/Master/APIVars.h
@@ -0,0 +1,17 @@
+//
+//  APIVars.h
+//  PlayingWith500px
+//
+//  Created by Julian Alonso on 8/4/15.
+//  Copyright (c) 2015 Julian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString *const BASE_POINT;
+extern NSString *const PHOTOS_END_POINT;
+extern NSString *const API_KEY;
+
+@interface APIVars : NSObject
+
+@end
