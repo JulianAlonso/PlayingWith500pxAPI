15e59799f4feb17e74bfa8dc7e755895d9490a59
Julian <julian.alonso.dev@gmail.com>
2015-04-09 14:29:47 +0200

    Added user ponso

---
+++ PlayingWith500px/PONSOS/User.h
@@ -0,0 +1,17 @@
+//
+//  User.h
+//  PlayingWith500px
+//
+//  Created by Julian Alonso on 9/4/15.
+//  Copyright (c) 2015 Julian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@property (nonatomic, copy) NSString *userId;
+@property (nonatomic, copy) NSString *userName;
+@property (nonatomic, copy) NSString *userAvatarURL;
+
+@end
---
+++ PlayingWith500px/PONSOS/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  PlayingWith500px
+//
+//  Created by Julian Alonso on 9/4/15.
+//  Copyright (c) 2015 Julian. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
