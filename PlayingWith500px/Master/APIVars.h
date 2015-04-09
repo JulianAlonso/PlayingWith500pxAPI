637515a87d8db422b40406dee2bd48cfcd8a03eb
Julian <julian.alonso.dev@gmail.com>
2015-04-09 15:07:18 +0200

    Added image size to request

--- PlayingWith500px/Master/APIVars.h
+++ PlayingWith500px/Master/APIVars.h
@@ -12,7 +12,10 @@ extern NSString *const BASE_POINT;
 extern NSString *const PHOTOS_END_POINT;
 extern NSString *const API_KEY;
 extern NSString *const PARAM_FEATURE;
+extern NSString *const PARAM_IMAGE_SIZE;
 extern NSString *const POPULAR_FEATURE;
+extern NSString *const IMAGE_SIZE_3;
+extern NSString *const IMAGE_SIZE_1600;
 
 @interface APIVars : NSObject
 
