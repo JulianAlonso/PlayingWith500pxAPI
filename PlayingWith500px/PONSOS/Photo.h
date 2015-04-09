6eae57cf8217ffe1dc42dd4bfff9f2dc7e7eff84
Julian <julian.alonso.dev@gmail.com>
2015-04-09 14:23:46 +0200

    Added properties to photo ponso

--- PlayingWith500px/PONSOS/Photo.h
+++ PlayingWith500px/PONSOS/Photo.h
@@ -10,10 +10,12 @@
 
 @interface Photo : NSObject
 
+@property (nonatomic, copy) NSString *photoId;
 @property (nonatomic, copy) NSString *photoName;
 @property (nonatomic, copy) NSString *photoDescription;
 @property (nonatomic, copy) NSString *photoCamera;
 @property (nonatomic, copy) NSString *photoAperture;
+@property (nonatomic, copy) NSString *photoRating;
 @property (nonatomic, copy) NSString *photoURL;
 
 @end
